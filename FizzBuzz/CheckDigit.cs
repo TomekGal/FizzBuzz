2284f6883662a909c9d73d54cb87a8c89722b8a5
TomekGal <75546362+TomekGal@users.noreply.github.com>
2021-01-07 21:01:24 +0100

    Add Project files

---
+++ FizzBuzz/CheckDigit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class CheckDigit
+    {
+        private int _digit;
+
+        public CheckDigit(int digit)
+        {
+            _digit = digit;
+           
+
+        }
+        public string Check()
+        {
+
+            if ((_digit % 3)==0 && (_digit % 5) == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+          
+            else if ((_digit % 3) == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if ((_digit % 5) == 0)
+            {
+                Console.WriteLine("Buzz");
+
+            }
+
+            string number = _digit.ToString();
+            return number;
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj Liczbę");
+                var liczba = Console.ReadLine();
+                // zmienna do przechowywania długość tekstu
+                var length = liczba.Length;
+
+                for (int i = 0; i < length; i++)
+                {
+                    char[] charFromString = liczba.ToCharArray();
+                    if (!char.IsDigit(liczba, i))
+                    { }
+                }
+
+                var liczbaint = int.Parse(liczba);
+
+                
+                var checkdigit = new CheckDigit(liczbaint);
+                var number = checkdigit.Check();
+
+                if ((liczbaint  % 3) != 0 && (liczbaint % 5) != 0)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine(number);
+                    Console.WriteLine("Podana liczba nie dzieli się przez 3 i 5 bez reszty");
+
+                }
+                
+            }
+
+
+            catch (Exception)
+            {
+
+                Console.WriteLine("Nie podałeś liczby");
+            }
+
+
+
+        }
+    }
+}
